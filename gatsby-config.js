e933aea7e4b0e093dee03de90e73717de3587360
Boris Rossovsky <Boris.Rozzy@gmail.com>
2020-08-04 01:36:23 -0400

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,63 @@
+module.exports = {
+  siteMetadata: {
+    title: `Title of app`,
+    description: `Description goes here.`,
+    author: `Author`,
+    siteUrl: `https://site.url`,
+  },
+  plugins: [
+    `gatsby-plugin-preact`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-catch-links`,
+    `gatsby-plugin-robots-txt`,
+    `gatsby-plugin-netlify`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://site.url`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Roboto`,
+            variants: [`100`, `700`],
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `default-starter`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        workboxConfig: {
+          globPatterns: ["**/*.{js,jsx,jpg,png,html,css}"],
+        },
+      },
+    },
+  ],
+}
