37cced47c9ce870608b0ede92a8103564969bcb2
Boris Rossovsky <boris.rozzy@gmail.com>
2020-08-05 00:33:19 -0400

    Minor styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,36 @@
-import React from 'react'
-import { PageProps } from 'gatsby'
+import React, { useState } from 'react'
+import styled from '@emotion/styled'
 
 import Layout from '@components/Layout'
 import SEO from '@components/SEO'
 
-const IndexPage: React.FC<PageProps> = () => (
+const FormConverter = styled.form`
+  position: relative;
+  max-width: 12rem;
+  margin: 0 auto;
+  top: 50vh;
+
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+
+  input[type='number'] {
+    -moz-appearance: textfield;
+  }
+`
+
+const IndexPage: React.FC = () => (
   <Layout>
-    <SEO title="Home" />
-    <form method="get" className="pure-g">
+    <SEO
+      title="Currency Converter"
+      description="A minimalistic currency converter"
+    />
+    <FormConverter method="post" className="pure-g">
       <input type="number" className="pure-u-1" />
       <input type="number" className="pure-u-1" />
-    </form>
+    </FormConverter>
   </Layout>
 )
 
