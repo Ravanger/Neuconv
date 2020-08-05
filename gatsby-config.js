26857f5a88dabd2a2b4649ca07ceda4453c56cc1
Boris Rossovsky <boris.rozzy@gmail.com>
2020-08-05 02:34:11 -0400

    useFetch custom hook

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,7 @@ module.exports = {
         aliases: {
           '@components': `src/components`,
           '@pages': `src/pages`,
+          '@hooks': `src/hooks`,
         },
         rootFolder: `src`,
       },
---
+++ src/hooks/useFetch.tsx
@@ -0,0 +1,63 @@
+// https://medium.com/swlh/usefetch-a-custom-react-hook-36d5f5819d8
+
+import { useState, useEffect } from 'react'
+
+type UseFetchPropTypes = {
+  url: RequestInfo
+  method?: string
+  bodyData?: any
+}
+
+type UseFetchReturnTypes = {
+  data: any
+  isLoading: boolean
+  hasError: boolean
+  errorMessage: string
+}
+
+const UseFetchDefaultProps = {
+  url: '',
+  method: 'GET',
+  bodyData: {},
+}
+
+const useFetch = (props: UseFetchPropTypes): UseFetchReturnTypes => {
+  const { url, method, bodyData } = props
+
+  const [data, setData] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
+  const [hasError, setHasError] = useState(false)
+  const [errorMessage, setErrorMessage] = useState('')
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true)
+      try {
+        const response = await fetch(url, {
+          method: method,
+          body: JSON.stringify(bodyData),
+        })
+        const result = await response.json()
+        if (response.ok) {
+          setData(result)
+        } else {
+          setHasError(true)
+          setErrorMessage(result)
+        }
+        setIsLoading(false)
+      } catch (err) {
+        setHasError(true)
+        setErrorMessage(err.message)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+    fetchData()
+  }, [])
+
+  return { data, isLoading, hasError, errorMessage }
+}
+
+useFetch.defaultProps = UseFetchDefaultProps
+
+export default useFetch
