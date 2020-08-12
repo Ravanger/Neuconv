f23cfcec536531ac33ae18ad5a06974e8b322739
Ravanger <Boris.Rozzy@gmail.com>
2020-08-12 02:14:13 -0400

    Add currency dropdowns; API

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,60 +1,61 @@
 module.exports = {
   siteMetadata: {
-    title: `Currency Converter`,
-    description: `A minimalistic currency converter.`,
-    author: `Boris Rossovsky`,
-    siteUrl: `https://www.borisr.dev`,
+    title: 'Currency Converter',
+    description: 'A minimalistic currency converter.',
+    author: 'Boris Rossovsky',
+    siteUrl: 'https://www.borisr.dev',
+    apiUrl: 'https://api.ratesapi.io/api/latest',
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sitemap`,
-    `gatsby-plugin-catch-links`,
-    `gatsby-plugin-robots-txt`,
-    `gatsby-plugin-emotion`,
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-catch-links',
+    'gatsby-plugin-robots-txt',
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
-      resolve: `gatsby-alias-imports`,
+      resolve: 'gatsby-alias-imports',
       options: {
         aliases: {
-          '@components': `src/components`,
-          '@pages': `src/pages`,
-          '@hooks': `src/hooks`,
+          '@components': 'src/components',
+          '@pages': 'src/pages',
+          '@hooks': 'src/hooks',
         },
-        rootFolder: `src`,
+        rootFolder: 'src',
       },
     },
     {
-      resolve: `gatsby-plugin-canonical-urls`,
+      resolve: 'gatsby-plugin-canonical-urls',
       options: {
-        siteUrl: `https://www.borisr.dev`,
+        siteUrl: 'https://www.borisr.dev',
       },
     },
     {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      resolve: 'gatsby-plugin-prefetch-google-fonts',
       options: {
         fonts: [
           {
-            family: `Roboto`,
-            variants: [`100`, `700`],
+            family: 'Roboto',
+            variants: ['100', '700'],
           },
         ],
       },
     },
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `default-starter`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        // icon: `src/images/gatsby-icon.png`,
+        name: 'default-starter',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        // icon: 'src/images/gatsby-icon.png',
       },
     },
     // {
-    //   resolve: `gatsby-plugin-offline`,
+    //   resolve: 'gatsby-plugin-offline',
     //   options: {
     //     workboxConfig: {
     //       globPatterns: ['**/*.{js,jsx,jpg,png,html,css}'],
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,24 @@
 import React, { useState } from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
 import styled from '@emotion/styled'
+
+import useFetch from '@hooks/useFetch'
 
 import Layout from '@components/Layout'
 import SEO from '@components/SEO'
 import Input from '@components/Input'
+import Select from '@components/Select'
 
 const FormConverter = styled.form`
   position: absolute;
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
+
+  label {
+    display: block;
+    text-align: right;
+  }
 
   input::-webkit-outer-spin-button,
   input::-webkit-inner-spin-button {
@@ -22,40 +31,91 @@ const FormConverter = styled.form`
 `
 
 const IndexPage: React.FC = () => {
+  const apiUrlData = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            apiUrl
+          }
+        }
+      }
+    `
+  )
+  const apiUrl: RequestInfo = apiUrlData.site.siteMetadata.apiUrl
+
   type StateTypes = {
-    convertFrom: number | undefined
-    convertTo: number | undefined
+    convertFromValue: number | undefined
+    convertToValue: number | undefined
+    convertFromCurrency: string | undefined
+    convertToCurrency: string | undefined
   }
 
   const [stateConvert, setStateConvert] = useState<StateTypes>({
-    convertFrom: undefined,
-    convertTo: undefined,
+    convertFromValue: undefined,
+    convertToValue: undefined,
+    convertFromCurrency: 'EUR',
+    convertToCurrency: 'CAD',
   })
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+  let currencyNamesArray: string[] = []
+  const { data, isLoading, hasError, errorMessage } = useFetch({
+    url: apiUrl,
+  })
+  if (hasError) {
+    console.error(errorMessage)
+  } else {
+    currencyNamesArray = data && Object.keys(data.rates)
+    currencyNamesArray && currencyNamesArray.push(data.base) //Add base
+    currencyNamesArray && currencyNamesArray.sort()
+  }
+
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ): void => {
     setStateConvert({
       ...stateConvert,
       [event.target.name]: event.target.value,
     })
   }
 
-  return (
+  return isLoading || !currencyNamesArray ? (
+    <p>Loading...</p>
+  ) : (
     <Layout>
       <SEO
         title="Currency Converter"
         description="A minimalistic currency converter"
       />
       <FormConverter method="post">
-        <Input
-          value={stateConvert.convertFrom}
-          name="convertFrom"
-          onChange={handleChange}
-        />
-        <Input
-          value={stateConvert.convertTo}
-          name="convertTo"
-          onChange={handleChange}
-        />
+        <label>
+          from:
+          <Input
+            value={stateConvert.convertFromValue}
+            name="convertFromValue"
+            onChange={handleChange}
+          />
+          <Select
+            name="convertFromCurrency"
+            currencynamesarray={currencyNamesArray}
+            value={stateConvert.convertFromCurrency}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          to:
+          <Input
+            value={stateConvert.convertToValue}
+            name="convertToValue"
+            onChange={handleChange}
+          />
+          <Select
+            name="convertToCurrency"
+            currencynamesarray={currencyNamesArray}
+            value={stateConvert.convertToCurrency}
+            onChange={handleChange}
+          />
+        </label>
       </FormConverter>
     </Layout>
   )
