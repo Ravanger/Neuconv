435b08c84dd51eede1ab6d7edbefeaa2bebfe97f
Ravanger <Boris.Rozzy@gmail.com>
2020-08-12 02:08:18 -0400

    Add Select component

---
+++ src/components/Select.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+type SelectProps = {
+  name: string
+  value: string | undefined
+  currencynamesarray: string[]
+  onChange?: (event: React.ChangeEvent<HTMLSelectElement>) => void
+}
+
+const Select = (props: SelectProps) => {
+  return (
+    <select {...props}>
+      {props.currencynamesarray &&
+        props.currencynamesarray.map((currencyName, index) => (
+          <option value={currencyName} key={index}>
+            {currencyName}
+          </option>
+        ))}
+    </select>
+  )
+}
+
+export default Select
